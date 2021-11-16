3b1587ee6f557d39977e9644cd8ab98cc696dca4
Emanuel Pires <emanuelpires@Emanuels-Air.home>
2021-11-16 08:25:40 -0800

    making navbar

--- health-cache-react-frontend/src/App.tsx
+++ health-cache-react-frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'reset-css';
+import Navbar from './components/Navbar';
+
+
 
 function App() {
   return (
     <div className="App">
-   
+   <Navbar/>
     </div>
   );
 }
