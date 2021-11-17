6133877861b1eb7d19caf319ffa6c96e90f04e5e
CharlesSmith98 <charles.w.smith@outlook.com>
2021-11-17 17:13:12 -0500

    Connected ClaimsLanding Component to our Claims Service

--- health-cache-react-frontend/src/components/claims/Claims.tsx
+++ health-cache-react-frontend/src/components/claims/Claims.tsx
@@ -1,18 +1,28 @@
 import { render } from '@testing-library/react';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './claims.css';
+import { IClaim } from './IClaim';
 
 
-const Claims: React.FC<any> = () => {
-    /*<div>
-      <p>Hello</p>
-    </div>*/
+const Claims: React.FC<any> = () => {  
+
+  let [claims, setClaims] = useState([]);
+  
+  const fetchClaims = async () => {
+    let res = await axios.get('http://localhost:8089/claim/all');
+    setClaims(res.data);
+  }
+
+  useEffect(() => {
+    fetchClaims();
+  },[claims.length])
+
   return (
     <div className="content">
       <div className="header-region">
-      <h2>My File Claims</h2>
+        <h2>My File Claims</h2>
         <button className="rev-btn" type="button">File New Claim</button>
-        
       </div>
       <hr />
       <table>
@@ -22,7 +32,21 @@ const Claims: React.FC<any> = () => {
           <th>Description</th>
           <th>Status</th>
         </tr>
-        <tr>
+        {
+          claims.map((claim:IClaim) => {
+            console.log('id: ' + claim.id);
+            console.log('claimType: ' + claim.claimType);
+            console.log('description: ' + claim.description);
+            console.log('status: ' + claim.status);
+            return (<tr>
+              <td>{claim.id}</td>
+              <td>{claim.claimType}</td>
+              <td>{claim.description}</td>
+              <td>{claim.status}</td>
+            </tr>);
+          })
+        }
+        {/*<tr>
           <td>1</td>
           <td>Surgery</td>
           <td>I need to take my face off!</td>
@@ -39,7 +63,7 @@ const Claims: React.FC<any> = () => {
           <td>Other</td>
           <td>Deny this one, dummy.</td>
           <td>DENIED</td>
-        </tr>
+        </tr>*/}
       </table>
     </div>
   );
---
+++ health-cache-react-frontend/src/components/claims/IClaim.ts
@@ -0,0 +1,7 @@
+export interface IClaim {
+    id: number,
+    userId: number,
+    claimType: string,
+    description: string,
+    status: string
+}
