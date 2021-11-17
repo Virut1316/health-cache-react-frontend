882d2911c44cf59b3e212915844cd7b44bbd33f7
CharlesSmith98 <charles.w.smith@outlook.com>
2021-11-17 13:35:29 -0500

    Created Mock Component for Claims Landing

--- health-cache-react-frontend/src/components/claims/Claims.tsx
+++ health-cache-react-frontend/src/components/claims/Claims.tsx
@@ -1,11 +1,47 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import './claims.css';
 
 
-const Claims: React.FC<any> = () => (
-    <div>
+const Claims: React.FC<any> = () => {
+    /*<div>
       <p>Hello</p>
+    </div>*/
+  return (
+    <div className="content">
+      <div className="header-region">
+      <h2>My File Claims</h2>
+        <button className="rev-btn" type="button">File New Claim</button>
+        
+      </div>
+      <hr />
+      <table>
+        <tr>
+          <th>Claim ID</th>
+          <th>Claim Type</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Surgery</td>
+          <td>I need to take my face off!</td>
+          <td>APPROVED</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Medication</td>
+          <td>I need cancer meds</td>
+          <td>PENDING</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Other</td>
+          <td>Deny this one, dummy.</td>
+          <td>DENIED</td>
+        </tr>
+      </table>
     </div>
   );
-
-  export default Claims;
+}
+export default Claims;
